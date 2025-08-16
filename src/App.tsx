c65ac9f4edf4dbc87fc1b12e2d6e23ef3c84ddc6
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2025-08-16 18:01:57 +0600

    implemented Redux basics including store setup, actions, reducer, counter slice, and handling action payloads

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import "./App.css";
+import { decrement, increment } from "./redux/features/counter/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hook";
+
+function App() {
+  // const dispatch = useDispatch();
+  const dispatch = useAppDispatch();
+  const { count } = useAppSelector((state) => state.counter);
+  // const { count } = useSelector((state: RootState) => state.counter);
+
+  console.log("count:", count);
+
+  /* const handleIncrement = (amount: number) => {
+    dispatch(increment(amount));
+  };
+   */
+
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
+
+  return (
+    <div>
+      <h1>Counter With Redux</h1>
+
+      <button onClick={() => dispatch(increment(5))}>Increment by 5</button>
+      <button onClick={() => dispatch(increment(1))}>Increment</button>
+      <div>{count}</div>
+      <button onClick={handleDecrement}>Decrement</button>
+    </div>
+  );
+}
+
+export default App;
